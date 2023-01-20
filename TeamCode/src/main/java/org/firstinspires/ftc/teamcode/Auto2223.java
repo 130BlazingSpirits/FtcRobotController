9c8c5e407fa559b344dd288215330de37d5e6f58
mebsport <56307645+mebsport@users.noreply.github.com>
2023-01-20 08:28:45 -0500

    Started working on base auto class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2223.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2223.java
@@ -3,16 +3,35 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-@Autonomous(name="Auto2223", group="auto")
+@Autonomous(name = "Auto2223", group = "auto")
 
 public class Auto2223 extends OpMode {
+    private Hardware hardware = new Hardware();
+    private RobotConfiguration robotConfiguration = null;
+    private boolean isRed = false;
+    private boolean isLeftStartingPos = false;
+
     @Override
     public void init() {
+        hardware.init(hardwareMap, this);
 
+        robotConfiguration = new RobotConfiguration();
+        robotConfiguration.readConfig();
+        isRed = robotConfiguration.isRed;
+        isLeftStartingPos = robotConfiguration.isLeftStartPos;
+
+        telemetry.addLine("Configuration Fetched");
+        telemetry.addData("Is Red?? ", isRed);
+        telemetry.addData("Is Left Position? ", isLeftStartingPos);
+        telemetry.update();
     }
 
     @Override
     public void loop() {
+        hardware.robo130.processCommands();
+    }
+
+    public void start(){
 
     }
 }
