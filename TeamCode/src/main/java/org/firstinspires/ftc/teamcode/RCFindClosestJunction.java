cb608620472c6cca054ea847be5a17a29ccbf690
mebsport <56307645+mebsport@users.noreply.github.com>
2023-02-16 13:56:33 -0500

    Update RCFindClosestJunction.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RCFindClosestJunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RCFindClosestJunction.java
@@ -4,7 +4,7 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-public class RCFindClosestJunction extends RobCommand{
+public class RCFindClosestJunction extends RobCommand {
     private final int FINDCLOSEJUNCTION = 0;
     private final int ROTATETOJUNCTION = 1;
     private final int DRIVETOJUNCTION = 2;
@@ -14,104 +14,99 @@ public class RCFindClosestJunction extends RobCommand{
     private double startTime = 0.0;
     private double timeout = 0.0;
 
-    public               double power = 0.5; //50% Speed
-    public               int targPosLF = 0;
-    public               int targPosLB = 0;
-    public               int targPosRF = 0;
-    public               int targPosRB = 0;
+    public double power = 0.5; //50% Speed
+    public int targPosLF = 0;
+    public int targPosLB = 0;
+    public int targPosRF = 0;
+    public int targPosRB = 0;
 
-    public RCFindClosestJunction(Hardware hardware, CVLocateClosestJunction pipeline, double timeout){
+    public RCFindClosestJunction(Hardware hardware, CVLocateClosestJunction pipeline, double timeout) {
         this.hardware = hardware;
         this.pipeline = pipeline;
         this.timeout = timeout;
     }
 
-    public void run(){
+    public void run() {
         startTime = hardware.getCurrentTime();
 
         hardware.webcam.setPipeline(pipeline);
-        hardware.webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        hardware.webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
+            public void onOpened() {
                 hardware.webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode)
-            {
+            public void onError(int errorCode) {
             }
         });
-        hardware.logMessage(false,"RCFindClosestJunction","Starting Check For Junctions");
+        hardware.logMessage(false, "RCFindClosestJunction", "Starting Check For Junctions");
     }
 
     @Override
     public boolean isComplete() {
 
-        if(hardware.getCurrentTime() - startTime > timeout){
+        if (hardware.getCurrentTime() - startTime > timeout) {
             hardware.webcam.stopStreaming();
             return true;
         }
 
-        switch(state){
+        switch (state) {
             case FINDCLOSEJUNCTION:
-                    if(pipeline.estimatedAngle >-999 )
-                    {
-                        hardware.logMessage(false,"RCFindClosestJunction","Junction Found");
-                        state = ROTATETOJUNCTION;
-                        runRotate(-1 * pipeline.estimatedAngle);
-                    }
+                if (pipeline.estimatedAngle > -999) {
+                    hardware.logMessage(false, "RCFindClosestJunction", "Junction Found");
+                    state = ROTATETOJUNCTION;
+                    runRotate(-1 * pipeline.estimatedAngle);
+                }
                 break;
 
             case ROTATETOJUNCTION:
-                if (Math.abs(hardware.motorLFront.getCurrentPosition() - targPosLF) < 15){
-                    hardware.logMessage(false,"RCFindClosestJunction","Rotate Complete, at requested position");
-                    if(pipeline.estimatedAngle > 15){ //Check if angle is close
-                        hardware.logMessage(false,"RCFindClosestJunction","Driving To Junction");
+                if (Math.abs(hardware.motorLFront.getCurrentPosition() - targPosLF) < 15) {
+                    hardware.logMessage(false, "RCFindClosestJunction", "Rotate Complete, at requested position");
+                    if (pipeline.estimatedAngle > 15) { //Check if angle is close
+                        hardware.logMessage(false, "RCFindClosestJunction", "Driving To Junction");
                         state = DRIVETOJUNCTION;
                         runDrive(pipeline.estimatedDistance - 5.5);
-                    }
-                    else{ //Wrong Angle
+                    } else { //Wrong Angle
                         state = FINDCLOSEJUNCTION;
                     }
                 }
                 break;
 
             case DRIVETOJUNCTION:
-                if (Math.abs(hardware.motorLFront.getCurrentPosition() - targPosLF) < 15){
-                    hardware.logMessage(false,"RCFindClosestJunction","Command Complete, at requested position");
+                if (Math.abs(hardware.motorLFront.getCurrentPosition() - targPosLF) < 15) {
+                    hardware.logMessage(false, "RCFindClosestJunction", "Command Complete, at requested position");
                     return true;
                 }
                 break;
         }
         return false;
     }
-    public void runRotate(double rotateBy){
-        hardware.logMessage(false,"RCFindClosestJunction","Starting Rotation" + rotateBy);
+
+    public void runRotate(double rotateBy) {
+        hardware.logMessage(false, "RCFindClosestJunction", "Starting Rotation" + rotateBy);
 
         int currPosLF = hardware.motorLFront.getCurrentPosition();
         int currPosLB = hardware.motorLBack.getCurrentPosition();
         int currPosRF = hardware.motorRFront.getCurrentPosition();
         int currPosRB = hardware.motorRBack.getCurrentPosition();
 
-        targPosLF = currPosLF - (int)(rotateBy / DriveTrain.DEGREE_PER_PULSE);
-        targPosLB = currPosLB - (int)(rotateBy / DriveTrain.DEGREE_PER_PULSE);
-        targPosRF = currPosRF + (int)(rotateBy / DriveTrain.DEGREE_PER_PULSE);
-        targPosRB = currPosRB + (int)(rotateBy / DriveTrain.DEGREE_PER_PULSE);
+        targPosLF = currPosLF - (int) (rotateBy / DriveTrain.DEGREE_PER_PULSE);
+        targPosLB = currPosLB - (int) (rotateBy / DriveTrain.DEGREE_PER_PULSE);
+        targPosRF = currPosRF + (int) (rotateBy / DriveTrain.DEGREE_PER_PULSE);
+        targPosRB = currPosRB + (int) (rotateBy / DriveTrain.DEGREE_PER_PULSE);
 
         hardware.motorLFront.setTargetPosition(targPosLF);
         hardware.motorLBack.setTargetPosition(targPosLB);
         hardware.motorRFront.setTargetPosition(targPosRF);
         hardware.motorRBack.setTargetPosition(targPosRB);
 
-        if(hardware.driveTrain.useSetVelocity){
-            hardware.motorLFront.setVelocity(power*DriveTrain.MAX_ANGULAR_VELOCITY);
-            hardware.motorLBack.setVelocity(power*DriveTrain.MAX_ANGULAR_VELOCITY);
-            hardware.motorRFront.setVelocity(power*DriveTrain.MAX_ANGULAR_VELOCITY);
-            hardware.motorRBack.setVelocity(power*DriveTrain.MAX_ANGULAR_VELOCITY);
-        }
-        else{
+        if (hardware.driveTrain.useSetVelocity) {
+            hardware.motorLFront.setVelocity(power * DriveTrain.MAX_ANGULAR_VELOCITY);
+            hardware.motorLBack.setVelocity(power * DriveTrain.MAX_ANGULAR_VELOCITY);
+            hardware.motorRFront.setVelocity(power * DriveTrain.MAX_ANGULAR_VELOCITY);
+            hardware.motorRBack.setVelocity(power * DriveTrain.MAX_ANGULAR_VELOCITY);
+        } else {
             hardware.motorLFront.setPower(power);
             hardware.motorLBack.setPower(power);
             hardware.motorRFront.setPower(power);
@@ -119,31 +114,30 @@ public class RCFindClosestJunction extends RobCommand{
         }
     }
 
-    public void runDrive(double moveBy){
-        hardware.logMessage(false,"RCFindClosestJunction","Starting Drive Forward " + moveBy);
+    public void runDrive(double moveBy) {
+        hardware.logMessage(false, "RCFindClosestJunction", "Starting Drive Forward " + moveBy);
 
         int currPosLF = hardware.motorLFront.getCurrentPosition();
         int currPosLB = hardware.motorLBack.getCurrentPosition();
         int currPosRF = hardware.motorRFront.getCurrentPosition();
         int currPosRB = hardware.motorRBack.getCurrentPosition();
 
-        targPosLF = currPosLF + (int)(moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
-        targPosLB = currPosLB + (int)(moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
-        targPosRF = currPosRF + (int)(moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
-        targPosRB = currPosRB + (int)(moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
+        targPosLF = currPosLF + (int) (moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
+        targPosLB = currPosLB + (int) (moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
+        targPosRF = currPosRF + (int) (moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
+        targPosRB = currPosRB + (int) (moveBy / DriveTrain.FORWARD_DISTANCE_PER_PULSE);
 
         hardware.motorLFront.setTargetPosition(targPosLF);
         hardware.motorLBack.setTargetPosition(targPosLB);
         hardware.motorRFront.setTargetPosition(targPosRF);
         hardware.motorRBack.setTargetPosition(targPosRB);
 
-        if(hardware.driveTrain.useSetVelocity){
-            hardware.motorLFront.setVelocity(power*DriveTrain.MAX_LINEAR_VELOCITY);
-            hardware.motorLBack.setVelocity(power*DriveTrain.MAX_LINEAR_VELOCITY);
-            hardware.motorRFront.setVelocity(power*DriveTrain.MAX_LINEAR_VELOCITY);
-            hardware.motorRBack.setVelocity(power*DriveTrain.MAX_LINEAR_VELOCITY);
-        }
-        else{
+        if (hardware.driveTrain.useSetVelocity) {
+            hardware.motorLFront.setVelocity(power * DriveTrain.MAX_LINEAR_VELOCITY);
+            hardware.motorLBack.setVelocity(power * DriveTrain.MAX_LINEAR_VELOCITY);
+            hardware.motorRFront.setVelocity(power * DriveTrain.MAX_LINEAR_VELOCITY);
+            hardware.motorRBack.setVelocity(power * DriveTrain.MAX_LINEAR_VELOCITY);
+        } else {
             hardware.motorLFront.setPower(power);
             hardware.motorLBack.setPower(power);
             hardware.motorRFront.setPower(power);
